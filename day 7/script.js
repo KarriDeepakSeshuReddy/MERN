aceabbea091a51f9cb6cef48bece7d714f2cd5de
deepak <deepaksahith418@gmail.com>
2025-06-24 11:28:47 +0530

    #2

--- day 7/script.js
+++ day 7/script.js
@@ -47,34 +47,104 @@
 
 */
 
-function login(cb) {
+// function login(cb) {
+//   console.log("User Logged in");
+//   cb();
+// }
+
+// function addItemToCart(cb) {
+//   console.log("Added items to cart");
+//   cb();
+// }
+
+// function createOrder(cb) {
+//   console.log("order creation");
+//   cb();
+// }
+
+// function payOrder(cb) {
+//   console.log("Payment Done..");
+//   cb();
+// }
+
+// function createOrderSummary(cb) {
+//   console.log("Created Order Summary");
+//   cb();
+// }
+
+// function updateWallet() {
+//   console.log("Update wallet");
+// }
+// login(()=>addItemToCart(()=>createOrder(()=>payOrder(()=>createOrderSummary(()=>updateWallet())))));
+
+//NOTE - Promises
+
+//Defination
+// Promise is an object which stores eventual completion or failure
+//  of an asynchronous code
+
+// function createOrder() {
+//   console.log("Order created successfully!");
+// }
+
+// const promise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     createOrder();
+//     resolve();
+//   }, 2000);
+// });
+
+// console.log(promise);
+
+//LINK - Callback hell via promise
+
+/*
+1. User Login
+2. Add items to cart
+3. Create Order
+4. Pay order
+5. Create Order summary
+6. Update wallet
+
+*/
+
+const login = new Promise((res, rej) => {
   console.log("User Logged in");
-  cb();
-}
+  res();
+});
 
-function addItemToCart(cb) {
+const addItemToCart = new Promise((res, rej) => {
   console.log("Added items to cart");
-  cb();
-}
+  res();
+});
 
-function createOrder(cb) {
-  console.log("order creation");
-  cb();
-}
+const createOrder = new Promise((res, rej) => {
+  console.log("Order creation");
+  res();
+});
 
-function payOrder(cb) {
+const payOrder = new Promise((res, rej) => {
   console.log("Payment Done..");
-  cb();
-}
-
-function createOrderSummary(cb) {
+  res();
+});
+const createOrderSummary = new Promise((res, rej) => {
   console.log("Created Order Summary");
-  cb();
-}
+  res();
+});
 
 function updateWallet() {
   console.log("Update wallet");
 }
-login(()=>addItemToCart(()=>createOrder(()=>payOrder(()=>createOrderSummary(()=>updateWallet())))));
 
-//NOTE -Promises
+login.then(() => {
+  addItemToCart.then(() => {
+    createOrder.then(() => {
+      payOrder.then(() => {
+        createOrderSummary.then(() => {
+          updateWallet();
+        });
+      });
+    });
+  });
+});
+
